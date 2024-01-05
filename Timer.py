0c8c5e690f29d0d351740018f41fdf831d0f19bf
Jayant Parashar <135538306+Jayantparashar10@users.noreply.github.com>
2024-01-05 10:46:31 +0530

    Create Timer.py

---
+++ Timer.py
@@ -0,0 +1,14 @@
+import time
+def contdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -=1
+    
+    print('Timer completed! ')
+
+t= input("Enter time in second ")
+
+contdown(int(t))
